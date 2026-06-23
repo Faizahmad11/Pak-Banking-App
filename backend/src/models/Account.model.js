41283a60532b33e44c40d3ae6dcb894efb3de579
Faiz Ahmad <faizahmad10102003@gmail.com>
2026-06-23 19:33:53 +0500

    update user model add account model

---
+++ backend/src/models/Account.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+const Schema = new mongoose.Schema({
+    user:{
+  type:mongoose.Schema.Types.ObjectId,
+  ref:'user',
+  required:true
+
+    },
+    amount:{
+         type:Number,
+         default:0
+    },
+    
+
+},{
+    timestamps:true
+})
